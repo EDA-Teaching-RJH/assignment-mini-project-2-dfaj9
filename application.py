330a86f2983a68eacdfb82db63743a7ec51ac383
dfaj9 <df339@kent.ac.uk>
2026-03-23 22:15:23 +0000

    User base class and validation checks NEW VERSION

--- application.py
+++ application.py
@@ -1,6 +1,17 @@
 class User:  #allows the user to be given a name
-    def __init__(self, name):
-        if not name:
-            raise ValueError("No name has been entered! ")
-        self.name = name
+    def __init__(self, username, password, email):
+        if not username: #These statements checks to see if information are missing
+            raise ValueError("A username is needed!") 
+        if not password:
+            raise ValueError("A password is needed!")
+        if not email:
+            raise ValueError("An Email is needed!")
         
+        self.username = username #saves the users username
+        self.password = password #saves the users password
+        self.email = email #saves the users email address
+
+    def __str__(self):
+        return f"{self.username} username,{self.email} email"  #Displays the username and email of a user
+
+       
