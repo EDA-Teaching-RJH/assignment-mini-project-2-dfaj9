6849ebfa7d81f1e47077f5108a093dbc81ce4d3f
dfaj9 <df339@kent.ac.uk>
2026-03-24 17:10:51 +0000

    I made this change so it can be easily tested

--- regex2.py
+++ regex2.py
@@ -1,12 +1,10 @@
 import re
 def valid_email(email):
         if re.search(r"^\w+@\w.+\.ac.uk$", email): #regex for common emails and uni emails
-            print("This email address is valid!")
-        else:
-            print("This email address is invalid! ")
+          return True
+        return False
 
 def strong_password(password):
     if re.search(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$", password):  #this does a lowercase, uppercase, special charachter, length and number check
-        print("This Password is strong!") #if user password has all, it is strong
-    else:
-        print("This Password is weak!")
+       return True
+    return False
