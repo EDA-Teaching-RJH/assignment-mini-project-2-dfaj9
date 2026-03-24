e798f092f020d7e5c45b95e05ca40bec3b5f1a26
dfaj9 <df339@kent.ac.uk>
2026-03-24 19:14:26 +0000

    client details saved as text file

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def sign_up():
     password = input("Password: ") #allows client to enter password
     email = input("Enter your email address: ") #allows client to enter email
 
-    user = Client(username, password, email)
+    user = Client(username, password, email) 
+    store_client(username, password, email) #saves the clients details into a text file
     print("Your account has now been created! ")
     print(user)
+def store_client(username, password, email):
+    with open("clients.txt", "a") as file: 
+        file.write(f"{username}, {password}, {email}\n")
 if __name__ == "__main__":
     sign_up()
+
+
+
